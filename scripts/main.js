84a2160470878ebc9265f5ca52eadec4b71a9b83
nadirefe <efenadir0@gmail.com>
2021-02-19 15:20:56 +0000

    finish hw

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,101 @@
 import data from "./data.js";
-import {searchMovieByTitle, makeBgActive} from "./helpers.js";
+import { searchMovieByTitle, makeBgActive } from "./helpers.js";
 
 class MoviesApp {
     constructor(options) {
-        const {root, searchInput, searchForm, yearHandler, yearSubmitter} = options;
+        const { root, searchInput, searchForm, yearHandler, genreHandler, yearSubmitter, genreSubmitter } = options;
         this.$tableEl = document.getElementById(root);
         this.$tbodyEl = this.$tableEl.querySelector("tbody");
-
         this.$searchInput = document.getElementById(searchInput);
-        this.$searchForm   = document.getElementById(searchForm);
+        this.$searchForm = document.getElementById(searchForm);
         this.yearHandler = yearHandler;
+        this.genreHandler = genreHandler;
         this.$yearSubmitter = document.getElementById(yearSubmitter);
+        this.$genreSubmitter = document.getElementById(genreSubmitter)
     }
 
-    createMovieEl(movie){
-        const {image, title, genre, year,id} = movie;
+    createMovieEl(movie) {
+        const { image, title, genre, year, id } = movie;
         return `<tr data-id="${id}"><td><img src="${image}"></td><td>${title}</td><td>${genre}</td><td>${year}</td></tr>`
     }
 
-    fillTable(){
-        /* const moviesHTML = data.reduce((acc, cur) => {
-            return acc + this.createMovieEl(cur);
-        }, "");*/
+    fillTable() {
         const moviesArr = data.map((movie) => {
-           return this.createMovieEl(movie)
+            return this.createMovieEl(movie)
         }).join("");
         this.$tbodyEl.innerHTML = moviesArr;
     }
+    /* YEAR CATEGORIES*/
+    getYearCategories() {
+        const yearArr = data.map(movie => movie.year)
+        const uniqueYearArr = [...new Set(yearArr)]
+        uniqueYearArr.sort((a, b) => b - a);
+        //array to json 
+        const res = uniqueYearArr.reduce((acc, curr) => (acc[curr] = '', acc), {});
+        uniqueYearArr.forEach(year => {
+            //count array objects
+            const count = yearArr.filter(item => item === year).length;
+            res[year] = count;
+        })
+        const $yearFilterParentEl = document.querySelector('.year-form-check');
 
-    reset(){
+        for (let val in res) {
+            const $categoryContainer = document.createElement('div');
+            $categoryContainer.className = "form-check";
+            const $el = document.createElement('input');
+            const $labelEl = document.createElement('label');
+            $el.className = "form-check-input"
+            $el.type = "radio"
+            $el.name = "year"
+            $el.id = val
+            $el.value = val
+            $labelEl.className = "form-check-label"
+            $labelEl.for = val
+            $labelEl.innerText = `${val} (${res[val]})`;
+            $categoryContainer.appendChild($el)
+            $categoryContainer.appendChild($labelEl)
+            $yearFilterParentEl.appendChild($categoryContainer)
+        }
+    }
+    /* GENRE CATEGORIES*/
+    getGenreCategories() {
+        const genreArr = data.map(movie => movie.genre)
+        const uniqueGenreArr = [...new Set(genreArr)]
+        const res = uniqueGenreArr.reduce((acc, curr) => (acc[curr] = '', acc), {});
+        uniqueGenreArr.forEach(year => {
+            //count array objects
+            const count = genreArr.filter(item => item === year).length;
+            res[year] = count;
+        })
+        const $genreFilterParentEl = document.querySelector('.genre-box');
+        const $genreButton = document.querySelector('.genre-button')
+        for (let val in res) {
+            const $categoryContainer = document.createElement('div');
+            $categoryContainer.className = "form-check";
+            const $el = document.createElement('input');
+            const $labelEl = document.createElement('label');
+            $el.className = "form-check-input"
+            $el.type = "checkbox"
+            $el.name = "genre"
+            $el.id = "flexCheckDefault"
+            $el.value = val
+            $labelEl.className = "form-check-label"
+            $labelEl.for = "flexCheckDefault"
+            $labelEl.innerText = `${val} (${res[val]})`;
+            $categoryContainer.appendChild($el)
+            $categoryContainer.appendChild($labelEl)
+            $genreFilterParentEl.insertBefore($categoryContainer, $genreButton);
+        }
+    }
+
+    reset() {
         this.$tbodyEl.querySelectorAll("tr").forEach((item) => {
             item.style.background = "transparent";
         })
     }
 
 
-    handleSearch(){
+    handleSearch() {
         this.$searchForm.addEventListener("submit", (event) => {
             event.preventDefault();
             this.reset();
@@ -43,11 +103,11 @@ class MoviesApp {
             const matchedMovies = data.filter((movie) => {
                 return searchMovieByTitle(movie, searchValue);
             }).forEach(makeBgActive)
-
+            this.$searchForm.reset();
         });
     }
 
-    handleYearFilter(){
+    handleYearFilter() {
         this.$yearSubmitter.addEventListener("click", () => {
             this.reset();
             const selectedYear = document.querySelector(`input[name='${this.yearHandler}']:checked`).value
@@ -57,10 +117,28 @@ class MoviesApp {
         });
     }
 
-    init(){
+    handleGenreFilter() {
+        this.$genreSubmitter.addEventListener('click', () => {
+            this.reset();
+            const selectedGenre = document.querySelectorAll(`input[name='${this.genreHandler}']:checked`)
+            let selectedGenreArr = Array.from(selectedGenre)
+            selectedGenreArr = selectedGenreArr.map(genre => genre.value);
+            selectedGenreArr.forEach(selectedGenre => {
+                const matchedGenres = data.filter((movie) => {
+                    return movie.genre === selectedGenre;
+                }).forEach(makeBgActive)
+
+            })
+        })
+    }
+
+    init() {
         this.fillTable();
         this.handleSearch();
         this.handleYearFilter();
+        this.getYearCategories();
+        this.getGenreCategories();
+        this.handleGenreFilter();
     }
 }
 
@@ -69,7 +147,9 @@ let myMoviesApp = new MoviesApp({
     searchInput: "searchInput",
     searchForm: "searchForm",
     yearHandler: "year",
-    yearSubmitter: "yearSubmitter"
+    genreHandler: "genre",
+    yearSubmitter: "yearSubmitter",
+    genreSubmitter: "genreSubmitter"
 });
 
 myMoviesApp.init();
